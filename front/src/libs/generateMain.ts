46b0a96f355f1861501be9d32f6470c03fa8a0bc
rafael.gonzalez <rafael.gonzalez@okmobilitygroup.com>
2021-07-02 13:00:39 +0200

    INITIAL ROUTER

--- front/src/libs/generateMain.ts
+++ front/src/libs/generateMain.ts
@@ -3,22 +3,15 @@ import {settings} from "../settings";
 
 export function generateMain(appName: string, appNameSerialize: string) {
     let generateTemplate = fs.readFileSync(`${settings.template}/initial/main.txt`, 'utf8')
-    let routerInitial = fs.readFileSync(`${settings.template}/initial/router.json`, 'utf8')
     let envInitial = fs.readFileSync(`${settings.template}/initial/env.txt`, 'utf8')
     let librariesInitial = fs.readFileSync(`${settings.template}/initial/libraries.txt`, 'utf8')
 
-    const routeInitialRS = JSON.parse(routerInitial).map(item => {
-        return `\tr.${item.method}("${item.route}", controllers.${item.routeName}())`;
-    }).join("\n");
+    generateTemplate = generateTemplate.replace('{generate}', "");
 
-    generateTemplate = generateTemplate.replace('{generate}', routeInitialRS);
-
-    if (routerInitial) {
-        generateTemplate = generateTemplate.replace(
-            "{libraries}",
-            `"${appNameSerialize}/controllers"\n\t"${appNameSerialize}/libs"\n${librariesInitial}`
-        )
-    }
+    generateTemplate = generateTemplate.replace(
+        "{libraries}",
+        `"${appNameSerialize}/routes"\n\t"${appNameSerialize}/libs"\n${librariesInitial}`
+    )
 
     fs.writeFileSync(`${appName}/main.go`, generateTemplate);
     fs.writeFileSync(`${appName}/.env`, envInitial);
--- front/src/libs/generateRouter.ts
+++ front/src/libs/generateRouter.ts
@@ -10,10 +10,29 @@ export function generateRouter(appName: string, appNameSerialize: string) {
         fs.writeFile(`${routeSettingApp}/router.json`, routerInitial, () => console.log("GENERATE ROUTE"));
     });
 
+    const template = fs.readFileSync(`${settings.template}/initial/initialRouter.txt`, 'utf-8');
+
+    const custom = template
+        .replace("{name}", "CustomRouter")
+        .replace("{children}", "")
+        .replace("{imports}", "");
+
+    const routeInitialRS = JSON.parse(routerInitial).map(item => {
+        return `\tr.${item.method}("${item.route}", controllers.${item.routeName}())`;
+    }).join("\n");
+
+    const generated = template
+        .replace("{name}", "GeneratedRouter")
+        .replace("{children}", routeInitialRS)
+        .replace("{imports}", `\t"${appNameSerialize}/controllers"`)
+
+    fs.writeFile(`${appName}/routes/generatedRouter.go`, generated, () => console.log("GENERATE ROUTE"));
+    fs.writeFile(`${appName}/routes/customRouter.go`, custom, () => console.log("GENERATE ROUTE"));
+
+
     JSON.parse(routerInitial).forEach(item => {
         let data = controller;
         data = data.replace("{name}", item.routeName);
-
         fs.writeFile(`${appName}/controllers/${item.routeName}.go`, data, () => {
 
         });
